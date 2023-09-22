747119859c7c58e7972e72a08b9fa972b95c1fd8
samuelchigozie <nzekwesammy@gmail.com>
2023-09-22 11:23:47 -0700

    lauching the two server files

---
+++ 0x0F-load_balancer/web-01.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#server 1 login commands
+
+#setting up SSH-AGENT
+eval $("ssh-agent")
+
+#path to dentity file
+ssh-add ~/.ssh/school
+
+# Path to identityFile
+ssh-add ~/.ssh/school
+
+# Username and IP Address 
+ssh ubuntu@100.26.50.147
